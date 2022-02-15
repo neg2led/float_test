01e6ae014f9b37f65b59db704280cbf3061a8412
neg2led <4232981+neg2led@users.noreply.github.com>
2022-02-15 15:44:09 +1100

    add f64 code

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,23 @@ fn main() {
     println!("rust float_test v{}", VERSION);
     let mut rng = thread_rng();
 
-    println!("Generating two random floats...");
-
+    println!("Generating two random f32s...");
     let x: f32 = rng.gen::<f32>();
     let y: f32 = rng.gen::<f32>();
-
     println!("x = {}, y = {}", x, y);
     let sum = x + y;
     let diff = x - y;
     let prod = x * y;
-
     println!("sum = {}, diff = {}, prod = {}", sum, diff, prod);
+
+    println!("Generating two random f64s...");
+    let x: f64 = rng.gen::<f64>();
+    let y: f64 = rng.gen::<f64>();
+    println!("x = {}, y = {}", x, y);
+    let sum = x + y;
+    let diff = x - y;
+    let prod = x * y;
+    println!("sum = {}, diff = {}, prod = {}", sum, diff, prod);
+
     println!("if this didn't crash, float is working! yay!");
 }
