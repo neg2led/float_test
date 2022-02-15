5cf19caf5bfbb6d7b31630ef01d13b95168c17ef
neg2led <4232981+neg2led@users.noreply.github.com>
2022-02-15 16:23:09 +1100

    ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@
 // Multi-language Perlin noise benchmark.
 // See https://github.com/nsf/pnoise for timings and alternative implementations.
 
-use std::f32::consts::PI;
-use rand::{Rng, thread_rng};
 use rand::seq::SliceRandom;
+use rand::{thread_rng, Rng};
+use std::f32::consts::PI;
 
 #[derive(Copy, Clone)]
 struct Vec2 {
@@ -21,13 +21,20 @@ struct Vec2 {
     y: f32,
 }
 
-fn lerp(a: f32, b: f32, v: f32) -> f32 { a * (1.0 - v) + b * v }
+fn lerp(a: f32, b: f32, v: f32) -> f32 {
+    a * (1.0 - v) + b * v
+}
 
-fn smooth(v: f32) -> f32 { v * v * (3.0 - 2.0 * v) }
+fn smooth(v: f32) -> f32 {
+    v * v * (3.0 - 2.0 * v)
+}
 
 fn random_gradient<R: Rng>(r: &mut R) -> Vec2 {
     let v = PI * 2.0 * r.gen::<f32>();
-    Vec2 { x: v.cos(), y: v.sin() }
+    Vec2 {
+        x: v.cos(),
+        y: v.sin(),
+    }
 }
 
 fn gradient(orig: Vec2, grad: Vec2, p: Vec2) -> f32 {
@@ -54,12 +61,14 @@ impl Noise2DContext {
         }
         permutations.shuffle(&mut rng);
 
-        Noise2DContext { rgradients: rgradients, permutations: permutations }
+        Noise2DContext {
+            rgradients: rgradients,
+            permutations: permutations,
+        }
     }
 
     fn get_gradient(&self, x: i32, y: i32) -> Vec2 {
-        let idx = self.permutations[(x & 255) as usize] +
-                    self.permutations[(y & 255) as usize];
+        let idx = self.permutations[(x & 255) as usize] + self.permutations[(y & 255) as usize];
         self.rgradients[(idx & 255) as usize]
     }
 
@@ -74,14 +83,24 @@ impl Noise2DContext {
         let x1 = x0 + 1;
         let y1 = y0 + 1;
 
-        ([self.get_gradient(x0, y0), self.get_gradient(x1, y0),
-          self.get_gradient(x0, y1), self.get_gradient(x1, y1)],
-         [Vec2 { x: x0f, y: y0f }, Vec2 { x: x1f, y: y0f },
-          Vec2 { x: x0f, y: y1f }, Vec2 { x: x1f, y: y1f }])
+        (
+            [
+                self.get_gradient(x0, y0),
+                self.get_gradient(x1, y0),
+                self.get_gradient(x0, y1),
+                self.get_gradient(x1, y1),
+            ],
+            [
+                Vec2 { x: x0f, y: y0f },
+                Vec2 { x: x1f, y: y0f },
+                Vec2 { x: x0f, y: y1f },
+                Vec2 { x: x1f, y: y1f },
+            ],
+        )
     }
 
     fn get(&self, x: f32, y: f32) -> f32 {
-        let p = Vec2 {x: x, y: y};
+        let p = Vec2 { x: x, y: y };
         let (gradients, origins) = self.get_gradients(x, y);
 
         let v0 = gradient(origins[0], gradients[0], p);
@@ -100,21 +119,21 @@ impl Noise2DContext {
 
 fn main() {
     let symbols = [' ', '░', '▒', '▓', '█', '█'];
-    let mut pixels = [0f32; 256*256];
+    let mut pixels = [0f32; 256 * 256];
     let n2d = Noise2DContext::new();
 
     for _ in 0..100 {
         for y in 0..256 {
             for x in 0..256 {
                 let v = n2d.get(x as f32 * 0.1, y as f32 * 0.1);
-                pixels[y*256+x] = v * 0.5 + 0.5;
+                pixels[y * 256 + x] = v * 0.5 + 0.5;
             }
         }
     }
 
     for y in 0..256 {
         for x in 0..256 {
-            let idx = (pixels[y*256+x] / 0.2) as usize;
+            let idx = (pixels[y * 256 + x] / 0.2) as usize;
             print!("{}", symbols[idx]);
         }
         print!("\n");
