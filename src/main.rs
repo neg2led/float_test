b029e3fda3cd0b6001aba4f86692e0c4debc2e25
neg2led <4232981+neg2led@users.noreply.github.com>
2022-02-15 16:51:06 +1100

    license info added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// SPDX-License-Identifier: MIT OR Apache-2.0
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Copyright 2014 The Rust Project Developers
+// Modifications Copyright 2022 Andrew Powers-Holmes <aholmes@omnom.net>
+//
 
 // Multi-language Perlin noise benchmark.
 // See https://github.com/nsf/pnoise for timings and alternative implementations.
